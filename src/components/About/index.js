b02a58789bb36edb9ebac513f264bd64e60c39fb
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-08-18 14:53:56 -0600

    fixed about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,10 +8,10 @@ function About() {
       <div className="my-2">
         <p>
         My name is Bryson Filosiliva Vatuvei. I was born in Las Vegas, Nevada on April 13, 2000. 
-        Although ive lived in a few states I grew up mainly in Utah. 
+        Although I've lived in a few states I grew up mainly in Utah. 
         Some values I hold very close to me are family and kindness. 
         I have worked in the construction field for the past 3 years, but I love technology so I wanted to work in something having to do with it.
-        I am currently in a full-stack coding bootcamp at the University of Utah, and I hope after I am done here I can continue to learn more about coding.
+        I have finished and earned my certificate from the University of Utah's full stack development bootcamp and I hope to get into the web developing industry!
         </p>
       </div>
     </section>
