8f83ef84cf1ef6d5ad252cbade32427a7ffa60eb
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-09-19 13:47:09 -0600

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,25 +4,26 @@ import gitHubImage from '../../assets/images/github.png'
 
 function Footer() {
     return (
-        <div>
-        <ul>
-        <li class="is-size-4">
+        <div className="flex-footer">
+        
+        <div class="is-size-4">
             Phone: <a href="tel:385.321.6033">385-321-6033</a> <br />
-        </li>
-        <li class="is-size-4">
+        </div>
+        
+        <div class="is-size-4 text-center">
             Email: <a href="mailto:vatuvei.bryson@yahoo.com">vatuvei.bryson@yahoo.com</a>
-        </li>    
-        <li class="is-size-4">
+        </div>    
+        <div class="is-size-4">
             GitHub: <a href="https://github.com/Bvatuvei" target="_blank">
                 <img className="nav-logo-image" src={gitHubImage} alt="" />
             </a>
-        </li>
-        <li class="is-size-4">
+        </div>
+        <div class="is-size-4">
             LinkedIn: <a href="https://www.linkedin.com/in/bryson-vatuvei-935aa0100/" target="_blank">
                 <img className="nav-logo-image" src={linkedInImage} alt="" />
             </a>
-        </li>
-    </ul>
+        </div>
+
     </div>
     )
 }
