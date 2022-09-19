c5a0815456f5add347cc6d7048fd7dcf49521b3f
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-09-19 14:03:50 -0600

    updated projects section

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -8,7 +8,7 @@ function Project() {
         <div class="card run-buddy-card">
           <div class="card-body project-information">
             <a href="https://bvatuvei.github.io/run-buddy/" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Run Buddy</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Run Buddy</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">Html/Css</h6>
             <a href="https://github.com/Bvatuvei/run-buddy/" class="card-link" target='_blank'>
@@ -25,7 +25,7 @@ function Project() {
         <div class="card stay-protected-card">
           <div class="card-body project-information">
             <a href="https://bvatuvei.github.io/Stay-Protected/" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Password Generator</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Password Generator</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">Javascript</h6>
             <a href="https://github.com/Bvatuvei/Stay-Protected/" class="card-link" target='_blank'>
@@ -42,7 +42,7 @@ function Project() {
         <div class="card taskmaster-card">
           <div class="card-body project-information">
             <a href="https://bvatuvei.github.io/taskmaster-pro/" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Taskmaster</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Taskmaster</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">Jquery/Bootstrap</h6>
             <a href="https://github.com/Bvatuvei/taskmaster-pro/" class="card-link" target='_blank'>
@@ -59,7 +59,7 @@ function Project() {
         <div class="card marvelous-gifs-card">
           <div class="card-body project-information">
             <a href="https://mjzabriskie.github.io/marvel-ous-gifs/" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Marvelous Gifs</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Marvelous Gifs</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">Group-Project/Team-Skills</h6>
             <a href="https://github.com/mjzabriskie/marvel-ous-gifs" class="card-link" target='_blank'>
@@ -76,7 +76,7 @@ function Project() {
         <div class="card fitness-tracker-card">
           <div class="card-body project-information">
             <a href="https://enigmatic-tor-51827.herokuapp.com/" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Fitness Tracker</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Fitness Tracker</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">Teamwork/All Back End Skills</h6>
             <a href="https://github.com/JaydenCowley/fitness-tracker" class="card-link" target='_blank'>
@@ -93,7 +93,7 @@ function Project() {
         <div class="card react-project-card">
           <div class="card-body project-information">
             <a href="https://bvatuvei.github.io/photo-port/" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Photo-Port</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Photo-Port</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">React/bootstrap</h6>
             <a href="https://github.com/Bvatuvei/photo-port" class="card-link" target='_blank'>
@@ -110,7 +110,7 @@ function Project() {
         <div class="card art-of-jenna-card">
           <div class="card-body project-information">
             <a href="https://artofjenna.herokuapp.com" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Art Of Jenna Louise</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Art Of Jenna Louise</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">React/MongoDB/Stripe</h6>
             <a href="https://github.com/attidack/Art-of-Jenna-Louise" class="card-link" target='_blank'>
@@ -127,7 +127,7 @@ function Project() {
         <div class="card book-search-card">
           <div class="card-body project-information">
             <a href="https://vast-stream-18159.herokuapp.com" class="card-link" target='_blank'>
-            <h5 class="card-title light-color fw-bold">Book Search</h5>
+            <h5 id="card-h5" class="card-title light-color fw-bold">Book Search</h5>
             </a>
             <h6 class="card-subtitle mb-2 light-color fw-bold">MERN</h6>
             <a href="https://github.com/Bvatuvei/Book-Search" class="card-link" target='_blank'>
